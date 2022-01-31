b2e3c56cd39b818c8db7c3cc9460a21645315d10
Aleksandr <elchaninov79@gmail.com>
2022-01-31 23:31:29 +0200

    ViewBinding в RecyclerItemViewHolder

--- app/src/main/java/com/elchaninov/gbprofessionaldevelopment/view/adapter/RecyclerItemViewHolder.kt
+++ app/src/main/java/com/elchaninov/gbprofessionaldevelopment/view/adapter/RecyclerItemViewHolder.kt
@@ -1,20 +1,20 @@
 package com.elchaninov.gbprofessionaldevelopment.view.adapter
 
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.elchaninov.gbprofessionaldevelopment.R
 import com.elchaninov.gbprofessionaldevelopment.databinding.ActivityMainRecyclerviewItemBinding
 import com.elchaninov.gbprofessionaldevelopment.model.data.DataModel
 
-class RecyclerItemViewHolder(viewBinding: ActivityMainRecyclerviewItemBinding) :
+class RecyclerItemViewHolder(private val viewBinding: ActivityMainRecyclerviewItemBinding) :
     RecyclerView.ViewHolder(viewBinding.root) {
 
     fun bind(data: DataModel) {
         if (layoutPosition != RecyclerView.NO_POSITION) {
-            itemView.findViewById<TextView>(R.id.header_textview_recycler_item).text = data.text
+            if (layoutPosition != RecyclerView.NO_POSITION) {
+                viewBinding.headerTextviewRecyclerItem.text = data.text
 
-            itemView.findViewById<TextView>(R.id.description_textview_recycler_item).text =
-                data.meanings?.get(0)?.translation?.translation
+                viewBinding.descriptionTextviewRecyclerItem.text =
+                    data.meanings?.get(0)?.translation?.translation
+            }
         }
     }
 }
