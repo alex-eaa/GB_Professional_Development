b30c9390f69a0b90ef20ee5ea0ef5083dc974d70
Aleksandr <elchaninov79@gmail.com>
2022-02-05 20:41:01 +0200

    Перименова некоторые функции конвертации

--- app/src/main/java/com/elchaninov/gbprofessionaldevelopment/model/datasource/RoomDataBaseImplementation.kt
+++ app/src/main/java/com/elchaninov/gbprofessionaldevelopment/model/datasource/RoomDataBaseImplementation.kt
@@ -1,26 +1,28 @@
 package com.elchaninov.gbprofessionaldevelopment.model.datasource
 
 import com.elchaninov.gbprofessionaldevelopment.model.data.DataModel
-import com.elchaninov.gbprofessionaldevelopment.model.datasource.room.EntityMeaning
-import com.elchaninov.gbprofessionaldevelopment.model.datasource.room.TranslationDao
-import com.elchaninov.gbprofessionaldevelopment.model.datasource.room.map
+import com.elchaninov.gbprofessionaldevelopment.model.datasource.room.*
 
 class RoomDataBaseImplementation(private val translationDao: TranslationDao) :
     DataSourceLocal<List<DataModel>> {
 
     override suspend fun getData(word: String): List<DataModel> =
         translationDao.getTranslationWhitsMeaning("$word%").map { translationWhitsMeaning ->
-            map(translationWhitsMeaning)
+            translationWhitsMeaning.toDataModel()
         }
 
-    override suspend fun saveData(list: List<DataModel>) {
-        val listForSave: MutableList<EntityMeaning> = mutableListOf()
-        list.map { dataModel ->
-            dataModel.meanings?.let {
-                listForSave.addAll(it.map { meaning -> map(meaning, dataModel) })
+    override suspend fun saveData(listDataModel: List<DataModel>) {
+        val listEntityTranslation =
+            listDataModel.map { dataModel -> dataModel.toEntityTranslation() }
+
+        val listEntityMeaning: MutableList<EntityMeaning> = mutableListOf()
+        listDataModel.map { dataModel ->
+            dataModel.meanings?.let { listMeaning ->
+                listEntityMeaning.addAll(listMeaning.map { meaning -> map(meaning, dataModel) })
             }
         }
-        translationDao.saveTranslationWhitsMeaning(list.map { map(it) }, listForSave)
+
+        translationDao.saveTranslationWhitsMeaning(listEntityTranslation, listEntityMeaning)
     }
 
     override suspend fun getTranslationFavorite(word: String): Boolean =
--- app/src/main/java/com/elchaninov/gbprofessionaldevelopment/model/datasource/room/UtilsMapper.kt
+++ app/src/main/java/com/elchaninov/gbprofessionaldevelopment/model/datasource/room/UtilsMapper.kt
@@ -5,12 +5,22 @@ import com.elchaninov.gbprofessionaldevelopment.model.data.Meanings
 import com.elchaninov.gbprofessionaldevelopment.model.data.Translation
 
 
-fun map(dataModel: DataModel): EntityTranslation {
-    return EntityTranslation(
-        id = dataModel.id,
-        text = dataModel.text
-    )
-}
+fun DataModel.toEntityTranslation(): EntityTranslation = EntityTranslation(
+    id = this.id,
+    text = this.text
+)
+
+fun TranslationWhitsMeaning.toDataModel(): DataModel = DataModel(
+    id = this.id,
+    text = this.text,
+    meanings = this.meanings?.map { entityMeaning -> entityMeaning.toMeanings() }
+)
+
+fun EntityMeaning.toMeanings(): Meanings = Meanings(
+    id = this.id,
+    imageUrl = this.imageUrl,
+    translation = Translation(this.text)
+)
 
 fun map(meanings: Meanings, dataModel: DataModel): EntityMeaning {
     return EntityMeaning(
@@ -18,22 +28,6 @@ fun map(meanings: Meanings, dataModel: DataModel): EntityMeaning {
         text = meanings.translation?.translation,
         imageUrl = meanings.imageUrl,
         translationId = dataModel.id
-    )
-}
-
-fun map(translationWhitsMeaning: TranslationWhitsMeaning): DataModel {
-    return DataModel(
-        id = translationWhitsMeaning.id,
-        text = translationWhitsMeaning.text,
-        meanings = translationWhitsMeaning.meanings?.map { map(it) }
-    )
-}
-
-fun map(entityMeaning: EntityMeaning): Meanings {
-    return Meanings(
-        id = entityMeaning.id,
-        imageUrl = entityMeaning.imageUrl,
-        translation = Translation(entityMeaning.text)
     )
 }
 
